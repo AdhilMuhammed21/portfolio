1d161c1efd19fb374f68b373a3497dc97d742757
Adhil Muhammed <adhilek46@gmail.com>
2024-10-18 00:00:22 +0530

    hero commit

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,18 +3,43 @@ import {name,work} from '../Constants/Constants'
 import { FaGithub } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { motion } from "framer-motion";
 
+const container = (delay) =>({
+  hidden:{y:100 ,opacity : 0 },
+  visible:{
+    y:0,
+    opacity:1,
+    transition:{duration: 0.5,delay: delay},
+  },
+});
 
 function Hero() {
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center overflow-y-hidden '>
-    <div className='md:border border-[#252525] bg-[#252525] flex flex-col justify-center px-12 md:px-32 py-6'>
-    <h1 className='text-[30px] md:text-[120px] text-[#CCD0D2] font-bold flex'>{name}</h1>
-    <p className='text-xs md:text-xl flex text-[#CCD0D2] '>{work}</p>
+    <motion.div
+    variants={container(0.2)}
+    initial='hidden'
+    animate='visible'
+     className='md:border border-[#252525] bg-[#252525] flex flex-col justify-center px-12 md:px-32 py-6'>
+    <motion.h1 
+    variants={container(0.3)}
+    initial='hidden'
+    animate='visible'
+     className='text-[30px] md:text-[120px] text-[#CCD0D2] font-bold flex'>{name}</motion.h1>
+    <motion.p 
+    variants={container(0.4)}
+    initial='hidden'
+    animate='visible'
+    className='text-xs md:text-xl flex text-[#CCD0D2] '>{work}</motion.p>
   
-    </div>
+    </motion.div>
 
-    <div className='mt-32  flex flex-row justify-center gap-6 md:gap-20'>
+    <motion.div 
+    variants={container(0.6)}
+    initial='hidden'
+    animate='visible'
+    className='mt-32  flex flex-row justify-center gap-6 md:gap-20'>
 
         <div className=' border border-[#CCD0D2] bg-[#252525] rounded-2xl px-4 py-4'>
         <FaGithub className=' text-4xl text-[#CCD0D2]'/>
@@ -25,7 +50,7 @@ function Hero() {
         <div className='  border border-[#CCD0D2] bg-[#252525] rounded-2xl  px-4 py-4'>
         <FaLinkedin className='text-4xl text-[#CCD0D2]'/>
         </div>
-    </div>
+    </motion.div>
     
     
     
--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,5 +1,5 @@
 export const name = `Adhil Muhammed`;
-export const work = `Designer/Developer`
+export const work = `Student/Developer`
 
 export const about = `Hello! I'm a Computer Science student with a passion for creating interactive and efficient web applications. With a focus on React development, I strive to build seamless user experiences that blend functionality and design. My journey in tech has given me a strong foundation in coding, problem-solving, and UI/UX design principles, enabling me to bring ideas to life.`
 
