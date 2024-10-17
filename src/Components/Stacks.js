04ff85979d0af4065fd9abece13cb75244bed510
Adhil Muhammed <adhilek46@gmail.com>
2024-10-17 22:19:57 +0530

    commit

--- src/Components/Stacks.js
+++ src/Components/Stacks.js
@@ -8,7 +8,7 @@ function Stacks() {
   return (
     <div className='border-b border-neutral-800 pb-24 '>
     <h2 className='my-20 text-center text-4xl text-[#CCD0D2] font-bold'>Technologies</h2>
-    <div className='flex flex-wrap items-center justify-center mx-16 gap-4 md:gap-20'>
+    <div className='flex flex-wrap items-center justify-center mx-20 gap-4 md:gap-20'>
         <div className='rounded-2xl border-4 border-[#CCD0D2] p-4'>
         <RiReactjsLine className='text-4xl text-cyan-400'/>
         </div>
