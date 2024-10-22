61f0c961fd65ce6169a87417efd8373c2b9141d9
Adhil Muhammed <adhilek46@gmail.com>
2024-10-22 19:43:22 +0530

    tech

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import { PROJECTS } from '../Constants/Constants'
+import { motion } from "framer-motion"
 
 function Projects() {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-      <h1 className='my-20 text-center text-[#CCD0D2] font-bold text-4xl'>Projects</h1>
-      <div>
+      <motion.h1
+      whileInView={{opacity : 1 , y:0}}
+      initial={{opacity:0 , y:100}}
+      transition={{duration : 0.5,delay:0.4}}
+      className='my-20 text-center text-[#CCD0D2] font-bold text-4xl'>Projects</motion.h1>
+      <motion.div
+      whileInView={{opacity : 1 , y:0}}
+      initial={{opacity:0 , y:100}}
+      transition={{duration : 0.5,delay:0.4}}
+      >
         {PROJECTS.map((project , index)=>(
           <div key={index}className='mb-8 flex flex-wrap lg:justify-center px-6' >
             <div className='w-full lg:w-1/4'>
-              <h2 className='text-[#CCD0D2]'>{project.title}</h2>
+              <h2 className='text-[#CCD0D2] text-bold pb-2'>{project.title}</h2>
             </div>
             <div className='w-full max-x-wl lg:w-3/4'>
               <p className='mb-4 text-neutral-400'>{project.description}</p>
+              {project.technologies.map((tech,index) => (
+                <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1'>{tech}</span>
+              ))}
             </div>
           </div>
 
         )
         )}
-      </div>
+      </motion.div>
     </div>
   )
 }
